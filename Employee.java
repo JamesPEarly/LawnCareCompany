96b8510507830d04de559a0b8812765d9d6bf23f
Mariana Avila <mavila@oswego.edu>
2020-02-10 20:55:33 -0500

    addes stuff in Employee

--- Employee.java
+++ Employee.java
@@ -1,1 +1,16 @@
 package LawnCareCompany;
+
+public class Employee{
+    private String name;
+    private int date;
+    private String status;
+    private int payRate;
+
+    public Employee(String name,int date,String status, int payrate){
+        this.name= name;
+        this.date= date;
+        this.status= status;
+        this.payRate= payrate;
+    }
+    
+}
