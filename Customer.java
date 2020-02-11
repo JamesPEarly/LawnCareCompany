292f5bc5692af2b6339d483650c5e874ec21989f
Mariana Avila <mavila@oswego.edu>
2020-02-10 20:47:31 -0500

    Added stuff in Customer

---
+++ Customer.java
@@ -0,0 +1,25 @@
+package LawnCareCompany;
+
+public Customer{
+    //Instance variables
+        private String name;
+        private String address;
+        private int lotSize;
+
+        public  Customer(String name, String address, int lotSize){
+                this.name=name;
+                this.address=address;
+                this.lotSize=lotSize;
+
+        }
+        public getName(){
+                return name;
+
+        }
+
+        public getaddress(){
+                return address;
+
+        }
+
+        }
