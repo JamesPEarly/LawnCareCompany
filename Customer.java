633a6cc5c9a6685535ad8c9489661863db92a0ac
JamesPEarly <james.early@oswego.edu>
2020-02-12 13:15:06 -0500

    Add associations

--- Customer.java
+++ Customer.java
@@ -5,7 +5,8 @@ public Customer{
         private String name;
         private String address;
         private int lotSize;
-
+    private Employee myContact;
+    
         public  Customer(String name, String address, int lotSize){
                 this.name=name;
                 this.address=address;
